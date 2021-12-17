91aaf2f1d5c913c721f171a50f1c8f20aae88382
Beatriz dos Anjos <89169695+beangzz@users.noreply.github.com>
2021-12-17 13:57:38 -0300

    Update 5 - List Overlap.py
    Atualização exercício 5

--- 5 - List Overlap.py
+++ 5 - List Overlap.py
@@ -1,35 +1,30 @@
-#and write a program that returns a list that contains only the elements that are common between the lists (without duplicates).
-# Make sure your program works on two lists of different sizes.
-# Extras:
-# Randomly generate two lists to test this
-# Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
+#Take two lists and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+#Extras:
+#Randomly generate two lists to test this
+
 
 import random
+
 a = []
 b = []
 
-tamanho1 = random.randint(1, 20)
-tamanho2 = random.randint(1, 20)
-
-
-while (len(a) < tamanho1):
+while (len(a) < random.randint(1, 20)):
     a.append(random.randint(0, 20))
 
-while (len(b) < tamanho2):
+while (len(b) < random.randint(1, 20)):
     b.append(random.randint(0, 20))
 
 a.sort()
 b.sort()
-print(a)
-print(b)
 
 lista = []
 
-for i in a:
-    if ((i in b) and (i not in lista)):
-        lista.append (i)
+for elemento in a:
+    if ((elemento in b) and (elemento not in lista)):
+        lista.append (elemento)
 
-
-lista.sort()
-
-print (lista)
+if (lista == []):
+    print("As listas não possuem elemenos em comum")
+else:
+    print("Elementos em comum: ", lista)

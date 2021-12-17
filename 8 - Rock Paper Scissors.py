e124a958163a0238bba4b7d6afea85fc50b0c46a
Beatriz dos Anjos <89169695+beangzz@users.noreply.github.com>
2021-12-17 14:21:16 -0300

    Update 8 - Rock Paper Scissors.py
    Atualização exercício 8

--- 8 - Rock Paper Scissors.py
+++ 8 - Rock Paper Scissors.py
@@ -1,5 +1,4 @@
-#Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input),
-# compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
+#Make a two-player Rock-Paper-Scissors game. (Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
 
 #Remember the rules:
 # Rock beats scissors
@@ -7,47 +6,63 @@
 # Paper beats rock
 
 ganhador = 0
+jogadas = ("PEDRA","PAPEL","TESOURA")
+
+pontos_jogador1 = 0
+pontos_jogador2 = 0
 
 print("Bem vindo")
 
 while (ganhador == 0):
-    jogada1 = input("Jogador 1, escolha sua jogada: ")
-    jogada2 = input ("Jogador 2, escolha sua jogada: ")
+    jogador1 = input("Jogador 1, escolha sua jogada: ").upper()
+    jogador2 = input("Jogador 2, escolha sua jogada: ").upper()
 
-    jogador1 = jogada1.upper()
-    jogador2 = jogada2.upper()
 
-    if (jogador1 == jogador2):
-        print("Empate!")
-        continue
-    elif (jogador1 == "PEDRA"):
-        if (jogador2 == "TESOURA"):
-            ganhador = 1
-        else:
-            ganhador = 2
-    elif (jogador1 == "TESOURA"):
-        if (jogador2 == "PAPEL"):
-            ganhador = 1
-        else:
-            ganhador = 2
-    elif (jogador1 == "PAPEL"):
-        if (jogador2 == "PEDRA"):
-            ganhador = 1
-        else:
-            ganhador = 2
-    else:
+    if (jogador1 not in jogadas or jogador2 not in jogadas):
         print("Jogada inválida, tente novamente")
         continue
+        
+    else:
+        if (jogador1 == jogador2):
+            print("Empate!")
+            continue
+
+        elif (jogador1 == "PEDRA"):
+            if (jogador2 == "TESOURA"):
+                ganhador = 1
+            else:
+                ganhador = 2
+
+        elif (jogador1 == "TESOURA"):
+            if (jogador2 == "PAPEL"):
+                ganhador = 1
+            else:
+                ganhador = 2
+
+        elif (jogador1 == "PAPEL"):
+            if (jogador2 == "PEDRA"):
+                ganhador = 1
+            else:
+                ganhador = 2
+
+
     if (ganhador == 1):
-        print("O jogador 1 mandou {}, enquanto o jogador 2 jogou {}, o ganhador foi o Jogaor 1".format(jogada1,jogada2))
+        print("O jogador 1 jogou {}, enquanto o jogador 2 jogou {}, o ganhador foi o Jogador 1".format(jogador1,jogador2))
+        pontos_jogador1 += 1
+
     elif (ganhador == 2):
-        print("O jogador 1 mandou {}, enquanto o jogador 2 jogou {}, o ganhador foi o Jogaor 2".format(jogada1,jogada2))
+        print("O jogador 1 jogou {}, enquanto o jogador 2 jogou {}, o ganhador foi o Jogador 2".format(jogador1,jogador2))
+        pontos_jogador2 += 1
 
     resposta = input("Deseja jogar novamente? ").upper()
+    
     if (resposta == "SIM"):
         ganhador = 0
         continue
-    else:
+    elif (resposta == "NAO" or resposta == "NÃO"):
         break
+    else:
+        ganhador = 0
 
+print("O jogador 1 fez {} pontos e o jogador 2 fez {} pontos".format(pontos_jogador1,pontos_jogador2))
 

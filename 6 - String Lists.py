4ae93c4106d472991c860da03979c46ffb0155cd
Beatriz dos Anjos <89169695+beangzz@users.noreply.github.com>
2021-12-17 14:00:24 -0300

    Update 6 - String Lists.py
    Atualização exercício 6

--- 6 - String Lists.py
+++ 6 - String Lists.py
@@ -1,11 +1,9 @@
 #Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
 
 palavra = input("Escreva: ").strip()
-palavra2 = []
-
-
 palavra = palavra.upper()
 
+palavra2 = []
 
 for i in palavra:
     palavra2.append(i)
@@ -15,13 +13,12 @@ palavra2.reverse()
 index = 0
 valor = False
 
-
 for i in palavra:
     if (i == palavra2[index]):
         valor = True
     else:
         valor = False
-    index = index+1
+    index = index + 1
 
 if (valor == True):
     print("{} é um palíndromo".format(palavra))

979aed723dc98aca2170ea57cdae269dcdd8941a
Beatriz dos Anjos <89169695+beangzz@users.noreply.github.com>
2021-12-17 13:45:19 -0300

    Update 3 - List Less Than Ten.py
    Atualizando exercício 3

--- 3 - List Less Than Ten.py
+++ 3 - List Less Than Ten.py
@@ -1,8 +1,11 @@
+#Take a list, say for example this one:
+#  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 #and write a program that prints out all the elements of the list that are less than 5.
-# Extras:
-# Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
-# Write this in one line of Python.
-# Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
+#Extras:
+#Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+#Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
 
 lista = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 lista_menor_5 = []
@@ -15,15 +18,20 @@ for i in lista:
     else:
         lista_maior_5.append(i)
 
+print("Os números da lista menores que 5 são: ", lista_menor_5)
+print("Os números da lista maiores que 5 são: ",lista_maior_5)
 
-numero = int(input("digite um número "))
+#~~~~~~~~
+        
+numero = int(input("Digite um número: "))
 menor_que_num = []
-maior_que_num=[]
+maior_que_num = []
+
 for i in lista:
     if (i < numero):
         menor_que_num.append(i)
     else:
         maior_que_num.append(i)
 
-print(menor_que_num)
-print(maior_que_num)
+print("Os números da lista menores que {} são: {}".format(numero,menor_que_num))
+print("Os números da lista maiores que {} são: {}".format(numero,maior_que_num))

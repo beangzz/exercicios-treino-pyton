85943c844c9b9c2720368d6de02d3c8d3080f1c9
Beatriz dos Anjos <89169695+beangzz@users.noreply.github.com>
2021-12-17 14:04:08 -0300

    Update 7 - List Comprehensions.py
    Atualização exercício 7

--- 7 - List Comprehensions.py
+++ 7 - List Comprehensions.py
@@ -1,10 +1,8 @@
-#Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
-# Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+#Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write a code that takes this list a and makes a new list that has only the even elements of this list in it.
 
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 pares = []
 impares = []
-
 
 for element in a:
     if (element%2 == 0):
@@ -12,5 +10,5 @@ for element in a:
     else:
         impares.append(element)
 
-print(pares)
-print(impares)
+print("Pares: ", pares)
+print("Impares: ", impares)

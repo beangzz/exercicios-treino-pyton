d5feed10758a951f671bf077d7e05d2ed0b39aac
Beatriz dos Anjos <89169695+beangzz@users.noreply.github.com>
2021-12-17 13:33:37 -0300

    Update 1 - Character Input.py
    Mudanças na formatação do que fo pedido

--- 1 - Character Input.py
+++ 1 - Character Input.py
@@ -1,22 +1,16 @@
-#Create a program that asks the user to enter their name and their age.
-#Print out a message addressed to them that tells them the year that they will turn 100 years old.
-#Extras:
+#Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
 
-#Ad on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
-#Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
-
+#Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. Print out that many copies of the previous message.
 
 nome = input("Insira seu nome:")
 idade = int(input("Insira sua idade:"))
 
-ano_atual=2021
+ano_atual = 2021
 
-diferenca = 100-idade
+diferenca = 100 - idade
 
-ano_100 = ano_atual+diferenca
+ano_100 = ano_atual + diferenca
 
 vezes = int(input("Quantas vezes você gostaria de imprimir?"))
 
-# Dava pra fazer : ~~for i in range(1,vezes+1):~~
 print(vezes * "{}, você completará 100 anos daqui a {} anos, no ano de {}!\n".format(nome,diferenca,ano_100))
-
